5f40b04f148917f535edfa0ca595b3306f2b5d17
Soborok1234 <sergei-goncharov2014@mail.ru>
2023-04-03 19:20:43 +0300

    Добавьте файлы проекта.

---
+++ GodVis/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GodVis
+{
+    public class Class1
+    {
+        public double GetArea(double a)
+        {
+            return a % 4;
+            
+        }
+    }
+}
